56fcbae1dc426be07e1f2914184f9ef018ef0079
EstefaniaR <a301605@uach.mx>
2022-10-25 12:05:03 -0600

    feat: se agregaron los docum

--- scripts/registro.js
+++ scripts/registro.js
@@ -1,6 +1,10 @@
-/*  */
+/* Se solicitan los docs */
 
 let formularioRegistro = document.querySelector(".form-registro");
+let inicioSesion = document.querySelector
+let cajaFrente = document.querySelector(".caja_frente");
+let cajaTras = document.querySelector(".caja_tras");
+let 
 
 function registro(){
     
