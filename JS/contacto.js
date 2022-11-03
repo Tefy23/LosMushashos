bff02e275fe69d7d461675dc99ac19344cccd702
Victor Lopez <victor.rodi25@gmail.com>
2022-11-03 11:28:28 -0600

    change: at the contac page

--- JS/contacto.js
+++ JS/contacto.js
@@ -1,1 +1,11 @@
-/* BOTON */
+/* Validaciones */
+
+const expresiones = {
+	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
+	asunto: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
+	mensaje: /^[a-z0-9_-]{20,500}$/,
+};
+
+
+/*  */
