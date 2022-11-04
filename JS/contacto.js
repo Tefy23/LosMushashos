426035d891653c14e7d1a7fa0050367d7405ac1c
Victor Lopez <victor.rodi25@gmail.com>
2022-11-03 18:35:07 -0600

    change: final validation on contact page

--- JS/contacto.js
+++ JS/contacto.js
@@ -3,37 +3,30 @@
 const expresiones = {
 	nombre: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
 	email: /^[a-zA-Z0-9_.+-]+@[a-zA-Z0-9-]+\.[a-zA-Z0-9-.]+$/,
-	asunto: /^[a-zA-ZÀ-ÿ\s]{1,40}$/,
 	mensaje: /^[a-z0-9_-]{20,500}$/,
 };
 
 
 const formularioContacto = document.querySelector('form');
-//console.log(formularioContacto)
 
 const inputsContacto = document.querySelectorAll('input', 'textarea');
 const textareaContacto = document.querySelector('textarea');
 
 const nombreError = document.querySelector('#errorNombre');
 const emailError = document.querySelector('#errorEmail');
-const asuntoError = document.querySelector('#errorAsunto');
 const mensajeError = document.querySelector('#errorMensaje');
 
 const statusInf = {
 	nombre: false,
 	email: false,
-	asunto: false,
 	mensaje: false,
 };
 
 inputsContacto.forEach((inp) => {
-	//console.log(inp);
 	inp.addEventListener('keyup', (e) => {
-		/* console.log(e.target.name) */
 
 		switch (e.target.name) {
 			case 'nombre':
-				/* console.log(e.target.value); */
 				if (expresiones.nombre.test(e.target.value)) {
 					statusInf.nombre = true;
 					nombreError.textContent = '';
@@ -48,15 +41,6 @@ inputsContacto.forEach((inp) => {
 				} else {
 					statusInf.email = false;
 					emailError.textContent = 'E-mail incorrecto';
-				}
-				break;
-			case 'asunto':
-				if (expresiones.asunto.test(e.target.value)) {
-					statusInf.asunto = true;
-					asuntoError.textContent = '';
-				} else {
-					statusInf.asunto = false;
-					asuntoError.textContent = 'Por favor escribe un asunto';
 				}
 				break;
 			case 'mensaje':
@@ -79,4 +63,6 @@ textareaContacto.addEventListener('input', (e) => {
 		(statusInf.mensaje = false), (mensajeError.textContent = 'Por favor escribe tu mensaje');
 	}
 });
+/*  */
+
 /*  */
